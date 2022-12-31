f195612736bc9fb9e3460cf4876ada6f38e19de6
Dawid <dawidnitka@gmail.com>
2022-12-31 05:53:07 +0100

    fix: container queries polyfill rework

--- src/helpers/loadCQ.ts
+++ src/helpers/loadCQ.ts
@@ -2,7 +2,7 @@ export default function loadCQ() {
     const script = document.createElement('script')
     
     setTimeout(() => {
-        script.src = 'https://cdn.jsdelivr.net/npm/container-query-polyfill@1/dist/container-query-polyfill.modern.js'
+        script.src = 'https://cdn.jsdelivr.net/npm/shadow-container-query-polyfill@1/dist/shadow-container-query-polyfill.modern.mjs'
     },
     500)
     document.getElementsByTagName('head')[0].appendChild(script)
